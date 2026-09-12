6931e25d7c6f90f945adf9e9ed224f431a971b22
Andrea Alberti <a.alberti82@gmail.com>
2026-09-12 19:13:12 +0200

    Version bumped to v1.0.0

--- lsp-ltex-plus.el
+++ lsp-ltex-plus.el
@@ -3,7 +3,7 @@
 ;; Author: Andrea Alberti <a.alberti82@gmail.com>
 ;; Maintainer: Andrea Alberti <a.alberti82@gmail.com>
 ;; Assisted-by: Claude:claude-opus-4-7
-;; Version: 0.6.0
+;; Version: 1.0.0
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: lsp, grammar, spelling, convenience
 ;; URL: https://github.com/ltex-plus/emacs-ltex-plus
